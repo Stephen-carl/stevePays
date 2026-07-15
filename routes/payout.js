42e18611db588454a61180cf83900d8e89c71cb4
Okafor Stephen <70633626+Stephen-carl@users.noreply.github.com>
2026-07-15 10:04:47 +0100

    Clean up payout.js by removing service payment routes
    Removed unused service payment routes from payout.js

--- routes/payout.js
+++ routes/payout.js
@@ -2,18 +2,12 @@ const express = require('express');
 const router = express.Router();
 const {
     thePayment, 
-    verifyTrans,
-    theServicePayment,
-    verifyServicePayment
+    verifyTrans
 } = require('../modules/smart-meter')
 
 //paystack routes
 router.post('/generateCheckoutSession', thePayment)
 router.post('/verifyTransCallback', verifyTrans)
-
-// for services
-router.post('/generateServiceCheckout', theServicePayment)
-router.post('/verifyServicePayment', verifyServicePayment)
 
 
 module.exports = router;
